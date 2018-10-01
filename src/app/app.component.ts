678d0f73705dac30f250fbbb91c5467172b7d8c2
MarilynWaldman <mdwaldman22@hotmail.com>
2018-10-01 17:13:21 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
+declare var google;
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'angular6MapWithMarkers';
+export class AppComponent  {
+  lat = 40.0150;
+  lng = -105.2705;
+  zoom = 10;
+  markers = [];
+  filteredMarkers = [];
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader
+  ) { }
+
+  getLocations(): Array<{ latitude: number, longitude: number }> {
+    return [
+      { 'latitude': 40.0150, 'longitude': -105.2705, 'label' : 'Mountains',  draggable: true},
+      { 'latitude': 40.0150, 'longitude': -105.99,   'label' : 'Boulder', draggable: true},
+      { 'latitude': 39.7392, 'longitude': -104.9903, 'label' : 'Denver', draggable: true },
+    ];
+  }
+
+  ngOnInit() {
+    this.markers = this.getLocations();
+
+    this.mapsAPILoader.load().then(() => {
+      const center = new google.maps.LatLng(this.lat, this.lng);
+      this.filteredMarkers = this.markers.filter(m => {
+        const markerLoc = new google.maps.LatLng(m.latitude, m.longitude);
+        const  distanceInKm = google.maps.geometry.spherical.computeDistanceBetween(markerLoc, center) / 1000;
+        if (distanceInKm < 100.0) {
+          return m;
+        }
+      });
+    });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA4jVrGqv7fXG2G2sJUziSS-1enptZq8xg',
+      libraries: ['geometry']
+    }), ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
